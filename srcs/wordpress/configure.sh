551fc2504f7330fe0f0b9c1d05b13af766fcc0a3
Jaeha Yi <jayi@c6r2s8.42seoul.kr>
2022-07-13 17:34:11 +0900

    wordpress를 index로 변경

--- srcs/wordpress/configure.sh
+++ srcs/wordpress/configure.sh
@@ -4,20 +4,10 @@ until mysql --host=$MYSQL_HOST --user=$WP_DATABASE_USR --password=$WP_DATABASE_P
   sleep 1
 done
 
-if [ ! -f "/var/www/html/index.html" ]; then
-    # static website
-    mv /tmp/index.html /var/www/html/index.html
-
-    mkdir /var/www/html/wordpress
-    cd /var/www/html/wordpress
-
-    wp core download --locale=ko_KR --allow-root
-    wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    # wp theme install inspiro --activate --allow-root
-
-fi
+wp core download --locale=ko_KR --allow-root
+wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 echo "Wordpress started on :9000"
 /usr/sbin/php-fpm8 -F -R
