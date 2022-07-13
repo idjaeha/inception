f3437a651ff12551f878b1e97b406d1a019fe5f5
Jaeha Yi <jayi@c6r2s8.42seoul.kr>
2022-07-13 18:26:17 +0900

    워드프레스 인덱스 루트 변경

--- srcs/wordpress/tools/configure.sh
+++ srcs/wordpress/tools/configure.sh
@@ -7,7 +7,7 @@ done
 cd /var/www/html
 wp core download --locale=ko_KR --allow-root
 wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 echo "Wordpress started on :9000"
