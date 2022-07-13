ba58f8ea547d6659f731b0e712234556d84aa2a5
Jaeha Yi <jayi@c6r2s8.42seoul.kr>
2022-07-13 18:23:40 +0900

    디렉토리 이동 명령어 추가

--- srcs/wordpress/tools/configure.sh
+++ srcs/wordpress/tools/configure.sh
@@ -4,6 +4,7 @@ until mysql --host=$MYSQL_HOST --user=$WP_DATABASE_USR --password=$WP_DATABASE_P
   sleep 1
 done
 
+cd /var/www/html
 wp core download --locale=ko_KR --allow-root
 wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
